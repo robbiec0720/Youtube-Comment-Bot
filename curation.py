cace238259450a8d68dd14e85b9c3291ba79657f
robbiec0720 <robbiec0720@gmail.com>
2023-01-23 15:06:15 -0600

    Created a framework for the program

---
+++ curation.py
@@ -0,0 +1,4 @@
+# import the csv created from extract.py
+# ask the user questions that will act as variables in the model
+# create a model that will curate the top 10 videos for the user
+# may convert this into a jupyter notebook
---
+++ extract.py
@@ -0,0 +1,4 @@
+# Create a list with all of the videoIds that you want to parse through
+# Create a dictionary that will store all the information you wish to extract from each video
+# Loop through each video and extract the information from each thread of comments and update the dictionary
+# Convert the dictionary into a csv file and export it
